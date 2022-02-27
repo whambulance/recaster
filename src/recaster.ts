bb031493050de06cf8423fa42642c595e9890d89
Jack Brooker <j.brooker04@gmail.com>
2022-02-27 13:36:21 +0000

    adding recaster class, beginning solve function

--- src/recaster.ts
+++ src/recaster.ts
@@ -1,1 +1,18 @@
-// recaster init
+import { Line } from '@/classes';
+import { Emitter, Receptor } from './interfaces';
+
+class Recaster {
+
+    public emitters: Emitter[] = [];
+    public receptors: Receptor[] = [];
+
+    public rays: Line[] = []
+
+    public solve (): void {
+        this.emitters.forEach((emitter: Emitter, key: number) => {
+
+        })
+    }
+}
+
+export { Recaster };
