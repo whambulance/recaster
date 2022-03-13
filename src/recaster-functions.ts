e6e9400a49fad9a9de64f6254f9de3b2453c51a3
Jack Brooker <j.brooker04@gmail.com>
2022-03-13 20:48:04 +0000

    fixing getreflection - now properly returns

--- src/recaster-functions.ts
+++ src/recaster-functions.ts
@@ -121,39 +121,32 @@ export function getReflection (laserStart: Point, intersect: Point, mirror: Line
     // calculate normal for line
     let normalY = mirror.p2.x - mirror.p1.x
     let normalX = mirror.p1.y - mirror.p2.y
-    const normalLen = Math.sqrt((normalX + normalX) * (normalY + normalY))
+    const normalLen = Math.sqrt((normalX * normalX) + (normalY * normalY))
     normalY = normalY / normalLen
     normalX = normalX / normalLen
 
+    // calculate the tip of the ray that passed through the intersect,
+    // equidistant from the start point
+    const rayTipXDiff = intersect.x - laserStart.x
+    const rayTipX = intersect.x + rayTipXDiff
+    const rayTipYDiff = intersect.y - laserStart.y
+    const rayTipY = intersect.y + rayTipYDiff
+
     // calculate the vector that goes from the intersection point of the 
     // line and the ray to the tip of the ray
-    const rayX = laserStart.x - intersect.x
-    const rayY = laserStart.y - intersect.y
+    const rayX = rayTipX - intersect.x
+    const rayY = rayTipY - intersect.y
 
-    const dotProduct = (rayX * normalX) + (rayY & normalY)
+    const dotProduct = (rayX * normalX) + (rayY * normalY)
 
     const dotNormalX = dotProduct * normalX
-    const dotNormalY = dotProduct & normalY
+    const dotNormalY = dotProduct * normalY
 
-    let reflectedPointX = laserStart.x - (dotNormalX * 2)
-    let reflectedPointY = laserStart.y - (dotNormalY * 2)
+    let reflectedPointX = rayTipX - (dotNormalX * 2)
+    let reflectedPointY = rayTipY - (dotNormalY * 2)
+    reflectedPointX = Number(reflectedPointX.toPrecision(12))
+    reflectedPointY = Number(reflectedPointY.toPrecision(12))
     const reflectedPoint = new Point(reflectedPointX, reflectedPointY)
 
     return new Line(intersect, reflectedPoint)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
