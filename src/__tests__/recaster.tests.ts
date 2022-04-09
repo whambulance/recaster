000c1fb7dee24976c5b8bbd7246f6baa0ae00587
Jack Brooker <j.brooker04@gmail.com>
2022-04-09 14:51:34 +0100

    updating recaster full environment test

--- src/__tests__/recaster.tests.ts
+++ src/__tests__/recaster.tests.ts
@@ -1,9 +1,8 @@
-// recaster tests
-
 import { Laser, Line, Mirror, Point } from "../classes"
+import { RayResolutions } from "../interfaces"
 import { Recaster } from '../recaster'
 
-const environments = [
+const dataset = [
     {
         name: 'Ray and two mirrors',
         emitters: [new Laser(new Point(3, 2), new Point(6, 2))],
@@ -11,29 +10,30 @@ const environments = [
             new Mirror(new Line(new Point(9, 0), new Point(13, 4))),
             new Mirror(new Line(new Point(9, 6), new Point(13, 10))),
         ],
-        rayPaths: [
-            [
-                new Line(new Point(3, 2), new Point(11, 2)),
-                new Line(new Point(11, 2), new Point(11, 8)),
-                new Line(new Point(11, 8), new Point(17, 8)),
-            ]
+        rayoutputs: [
+            {
+                rays: [
+                    new Line(new Point(3, 2), new Point(11, 2)),
+                    new Line(new Point(11, 2), new Point(11, 8)),
+                    new Line(new Point(11, 8), new Point(31.14213562373095, 8)),
+                ],
+                resolution: RayResolutions.Infinity,
+            }
         ],
     },
 ]
 
 describe('recaster full envrionment tests', () => {
-    environments.forEach(env => {
-        test(`test - ${env.name}`, () => {
-
-            console.log('emitters', JSON.stringify(env.emitters))
-
+    it.each(dataset)(
+        '$name',
+        ({ emitters, receptors, rayoutputs}) => {
             let recaster = new Recaster
-            recaster.emitters = env.emitters
-            recaster.receptors = env.receptors
+            recaster.emitters = emitters
+            recaster.receptors = receptors
 
-            recaster.solve()
-            console.log('inited raypaths', JSON.stringify(recaster.rayPaths))
+            const output = recaster.solve()
 
-        })
-    })
+            expect(output).toStrictEqual(rayoutputs)
+        }
+    )
 })
