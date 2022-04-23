a06ac6f68cf7695679a9ad9cee70b02ba118119f
Jack Brooker <j.brooker04@gmail.com>
2022-04-23 12:35:42 +0100

    moved to its own file

---
+++ src/classes/point.ts
@@ -0,0 +1,14 @@
+export class Point {
+    x: number;
+    y: number;
+
+    /**
+     * Define a 2d point in space
+     * @param x The x-coordinate of the point
+     * @param y The y-coordinate of the point
+     */
+    constructor (x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
