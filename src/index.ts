15cf445b64a5339649a50b10b8529d7c2319e1b8
Jack Brooker <j.brooker04@gmail.com>
2022-04-23 13:19:29 +0100

    adding base index export

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Laser } from '@/emitters';
+import { Mirror, Absorber } from '@/receptors';
+import { Ray } from '@/classes';
+import { Recaster } from '@/recaster';
+
+module.exports = {
+  Laser,
+  Mirror,
+  Absorber,
+  Ray,
+  Recaster,
+};
