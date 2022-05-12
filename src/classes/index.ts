0867ef34c33cd102443cf7c9e7441b1b5c980204
Jack Brooker <j.brooker04@gmail.com>
2022-05-12 15:52:02 +0100

    exporting new classes rectangle, triangle, circle, and vector

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -3,3 +3,7 @@ export * from './boundsgroup';
 export * from './line';
 export * from './point';
 export * from './ray';
+export * from './rectangle';
+export * from './triangle';
+export * from './circle';
+export * from './vector';
